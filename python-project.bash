9bbb387e98a4f905eb991f196671fc256f9accf1
= <=>
2023-11-06 18:13:46 -0500

    Set up git support

--- python-project.bash
+++ python-project.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 read -r -p "project name: " NAME
+read -r -p "github user name: " GITHUB_USERNAME
 
-mkdir ~/Documents/coding-projects/"$NAME"
+# Make folder
+DIRECTORY=~/Documents/coding-projects/"$NAME"
+mkdir "$DIRECTORY"
+
+
+# Set up git
+cd "$DIRECTORY" || exit
+git init
+
+# Set up Github
+gh repo create "$NAME" --public
+git remote add orgin https://github.com/"$GITHUB_USERNAME"/"$NAME".git
+git push -u origin master
