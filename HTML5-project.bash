2d0e4845f9f47202dc82b4c7fa3c41d5a497fa24
= <=>
2023-11-07 09:58:42 -0500

    added HTML5 project support

---
+++ HTML5-project.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -r -p "project name: " NAME
+read -r -p "github user name: " GITHUB_USERNAME
+
+# Make folder
+DIRECTORY=~/Documents/coding-projects/"$NAME"
+mkdir "$DIRECTORY"
+touch "$DIRECTORY"/index.html
+touch "$DIRECTORY"/script.js
+touch "$DIRECTORY"/style.css
+cp index.html "$DIRECTORY"
+
+
+# Set up git
+cd "$DIRECTORY" || exit
+git init
+git add "*"
+git commit --message "init"
+
+# Set up Github
+gh repo create "$NAME" --public
+git remote add orgin https://github.com/"$GITHUB_USERNAME"/"$NAME".git
+git push -u origin master
+
+# Open in VSCode
+
+# NOTE: if you use Flatpak version of VSCode or VSCodium, you need an alias of code to run it
+code "$DIRECTORY"
